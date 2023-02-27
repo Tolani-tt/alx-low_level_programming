9cc557999b6baceaaa5c145f9084ffedc79daef6
Tolani <tolaniidiah@gmail.com>
2023-02-27 13:03:23 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,25 +4,24 @@
 
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98
+ * print_98 - Prints all natural numbers from n to 98
  * @n: The number to start printing from
  * Return: Always 0.
- */
+*/
 
 void print_to_98(int n)
-
-{
-
-	if (n <= 98)
-	{
-	for (; n <= 98; n++)
-	{
-	if (n == 98)
-	{
-	printf("%d", n);
-	printf("\n");
-	break;
-	}
+        
+{       
+        if (n <= 98)
+        {       
+        for (; n <= 98; n++)
+        {       
+        if (n == 98)
+        {
+        printf("%d", n);
+        printf("\n");
+        break;
+        }
 	else
 	{
 	printf("%d, ", n);
@@ -35,7 +34,7 @@ void print_to_98(int n)
 	{
 	if (n == 98)
 	{
-	printf("%d", n);
+        printf("%d", n);
 	printf("\n");
 	break;
 	}
