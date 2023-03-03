906e524c2c7ba490725a73d7dcc82cd0fd4021de
Tolani <tolaniidiah@gmail.com>
2023-03-03 09:53:56 -0800

    101-natural.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,18 +10,18 @@
 */
 
 void print_to_98(int n)
-        
-{       
-        if (n <= 98)
-        {       
-        for (; n <= 98; n++)
-        {       
-        if (n == 98)
-        {
-        printf("%d", n);
-        printf("\n");
-        break;
-        }
+
+{
+	if (n <= 98)
+	{
+	for (; n <= 98; n++)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
 	else
 	{
 	printf("%d, ", n);
@@ -34,7 +34,7 @@ void print_to_98(int n)
 	{
 	if (n == 98)
 	{
-        printf("%d", n);
+	printf("%d", n);
 	printf("\n");
 	break;
 	}
