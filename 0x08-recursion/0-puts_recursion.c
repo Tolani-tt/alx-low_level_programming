04db778624a54340551873643b468bca17aff001
Tolani <tolaniidiah@gmail.com>
2023-03-18 11:01:03 -0700

    sk9'`

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*S == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 	
-	_putchar(*S);
-	S++;
+	_putchar(*s);
+	s++;
 	_puts_recursion(s);
 }	
