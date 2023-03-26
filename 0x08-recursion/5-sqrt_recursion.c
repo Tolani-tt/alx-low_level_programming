edfe3cfbc175dde23d7f282a3691665c7092fb92
Tolani <tolaniidiah@gmail.com>
2023-03-26 04:03:43 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -19,7 +19,7 @@ int _sqrt_recursion(int n)
  * @n: number to calculate the square root
  * @i: iterate number
  *
- * Return: thenatural square root
+ * Return: the natural square root
  */
 int _sqrt(int n, int i)
 {
@@ -29,6 +29,6 @@ int _sqrt(int n, int i)
 
 	if (sqrt == n)
 		return (i);
-i
+
 	return (_sqrt(n, i + 1));
 }
